2c0625938c3a7366fe907721e3f46b3d33212b6e
1U24IT026-DEVADHARSHINI <devadharshini1838@gmail.com>
2026-09-11 12:35:16 +0530

    Update firewall_services.sh

--- firewall_services.sh
+++ firewall_services.sh
@@ -1,42 +1,43 @@
-#!/bin/bash
+!/bin/bash
 
 # ==========================================
 # Managing Firewall Services using firewalld
 #
-# Student Name:
-# Roll Number:
+# Student Name:devadharshini.j
+# Roll Number:1U24IT026
 # ==========================================
 
 
 # Check current firewall services
-
+firewall-cmd --list-services
 
 
 
 # Allow HTTP service
-
+firewall-cmd --add-service=http
 
 
 # Allow HTTPS service
+firewall-cmd --add-service=https
+
 
 
 
 
 # Display enabled services
-
-
+firewall-cmd --list-services
 
 
 # Remove HTTP service
+firewall-cmd --remove-service=http
 
 
 
 
 # Allow SSH permanently
-
+firewall-cmd --add-service=ssh --permanent
 
 
 
 # Reload firewall configuration
-
-
+firewall-cmd --reload
